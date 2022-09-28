797dbc10f5b09c80f7c9e0c3f3659556d17aaa87
Malik <willis.malik.r@gmail.com>
2022-09-28 13:37:32 -0500

    Bonus solution completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -19,21 +19,19 @@ public class ConsoleExercises {
 //        System.out.printf( "%s%n%s%n%s%n", first_word, second_word, third_word);
 
         System.out.println("Enter a sentence:");
-        String userSentence = scanner.next();
+        String userSentence = scanner.nextLine();
         System.out.println(userSentence);
 
         scanner.nextLine();
         System.out.println("What is the length of the classroom");
         System.out.println("What is the length?");
-        int length = scanner.nextInt();
+        double length = scanner.nextInt();
         System.out.println("What is the width?");
-        int width = scanner.nextInt();
-        int perimeter, area;
+        double width = scanner.nextInt();
+        double perimeter, area;
         area = length * width;
         perimeter = (length * 2) + width * 2;
-        System.out.printf("The perimeter of codeup's classroom is %d.%n", perimeter);
-        System.out.printf("The area of codeup's classroom is %d.%n", area);
-
-
+        System.out.printf("The perimeter of codeup's classroom is %.2f%n", perimeter);
+        System.out.printf("The area of codeup's classroom is %.2f%n", area);
     }
 }
