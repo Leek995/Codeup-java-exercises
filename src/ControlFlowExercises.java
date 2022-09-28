905347c346a5565c875043867b955bb2acb3ff40
Malik <willis.malik.r@gmail.com>
2022-09-28 16:24:21 -0500

    Solution 2 FizzBizz solved

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -12,5 +12,22 @@ public class ControlFlowExercises {
             a += 2;
             System.out.printf("the current iteration of a is... %d.%n", a);
         }while (a < 100);
+
+        int x = 0;
+        while (x < 100){
+            ++x;
+            if(x % 3 == 0 ){
+                if (x % 5 == 0){
+                    System.out.println("FizzBuzz");
+                    continue;
+                }
+                System.out.println("Fizz");
+                continue;
+            }else if(x % 5 == 0){
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.printf("%d%n", x);
+        }
     }
 }
