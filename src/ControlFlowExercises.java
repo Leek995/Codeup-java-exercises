70a0bfb1bbec027f328393dbad4dd005f921652b
Malik <willis.malik.r@gmail.com>
2022-09-28 16:05:49 -0500

    soltuion on refactored

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
         int i = 5;
-        while (i < 15){
-            System.out.printf("The current iteration of i is: %d%n", i);
+        while (i <= 15){
+//            System.out.printf("The current iteration of i is: %d%n", i);
+            System.out.printf("%d ", i);
             ++i;
         }
     }
