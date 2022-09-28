c229d09d9e415ee8d1e38e8e36cff19587c5fb78
Malik <willis.malik.r@gmail.com>
2022-09-28 16:02:35 -0500

    solution 1 using while loop complete

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,9 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        int i = 5;
+        while (i < 15){
+            System.out.printf("The current iteration of i is: %d%n", i);
+            ++i;
+        }
+    }
+}
