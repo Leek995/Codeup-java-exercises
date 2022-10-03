33d9c40c71e441b90e5b170ac8b85c2f72e72e39
Malik <willis.malik.r@gmail.com>
2022-10-03 11:37:10 -0500

    multipication

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -2,6 +2,7 @@ public class MethodsExercise {
     public static void main(String[] args){
         System.out.println(addition(3, 5));
         System.out.println(subtraction(3,5));
+        System.out.println(multiplication(3,5));
     }
     public static int addition(int number_1, int number_2){
         return number_1 + number_2;
@@ -9,4 +10,8 @@ public class MethodsExercise {
     public static int subtraction(int number_1, int number_2){
         return number_1 - number_2;
     }
+    public static int multiplication(int number_1, int number_2){
+        return number_1 * number_2;
+    }
+
 }
